d42f4547669300e43fab3502e354c842dbbb06f2
Joep Kockelkorn <joepkockelkorn@gmail.com>
2024-04-13 15:51:40 +0200

    fix: routing in github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 installGlobals();
 
@@ -9,6 +11,16 @@ export default defineConfig({
   plugins: [
     remix({
       ssr: false,
+      basename: "/system-designer/",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html")
+        );
+      },
     }),
     tsconfigPaths(),
   ],
